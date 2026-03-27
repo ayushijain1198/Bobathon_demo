583fecff6f3627e08a2c044a437f0b7a6e0c29c0
PradumnyaZ <pradumnya.zendphale@ibm.com>
2026-03-27 16:25:31 +0000

    Add TODO comment for input validation in BookController

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Book;
 import com.example.demo.service.BookService;
 
+// TODO: Add input validation for book creation
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
